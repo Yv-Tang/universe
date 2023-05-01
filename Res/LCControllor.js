d4674a40d8d092334a2f3d521371101a77fdc322
universe <ronglightyv@qq.com>
2023-05-01 13:06:21 +0800

    Update

--- Res/LCControllor.js
+++ Res/LCControllor.js
@@ -37,16 +37,35 @@ function getComments(todays) {
     var days = todays;
     query.descending('createdAt'); // 较晚发布的在上
     query.find().then((res) => {
-        //元素
+        //元素操作
         for (var key in res) {
-            var portrait = "//q.qlogo.cn/g?b=qq&nk=" + res[key].name + "&s=100"
+            // 数据储存
+            var portrait = `//q.qlogo.cn/g?b=qq&nk=${res[key].get('name')}&s=100`;
+            var Qname;
+            qNameGeter(res[key].get('name'));
+            var time = `${res[key].getcreatedAt}`;
+            var content = `${res[key].get('content')}`
+            // 操作
+            var divElement = document.createElement('div');
+            divElement.innerHTML = `
+            <div id=${key}>
+                <div class="head-box">
+                    <img id="_portrait" src=${portrait}>
+                    <span id="_name">${Qname}<br></span>
+                    <span id="_time">${time}</span>
+                </div>
+                <div class="body-box">
+                    <div id="_content">${content}</div>
+                </div>
+            </div>
+            `;
+            $('-1').appendChild(divElement);
         }
 
         //调试
         console.warn('***********************\n res内容')
         console.table(res.map(item => item.toJSON()));
 
-        
     });
 };
 
@@ -63,3 +82,40 @@ function getComments(todays) {
 function $(Nid) {
     return document.getElementById(Nid);
 }
+
+/**
+ * 获取QQ昵称
+ * @param {any} Qnumber - QQ账号
+ */
+function qNameGeter(Qnumber) {
+    // 动态创建 script 标签并指定 URL 和回调函数名称
+    var QnameGeter = document.createElement('script');
+    QnameGeter.src = `https://r.qzone.qq.com/fcg-bin/cgi_get_portrait.fcg?uins=${Qnumber}`;
+    // 将 script 标签添加到页面中以触发请求
+    document.body.appendChild(QnameGeter);
+    return portraitCallBack();
+}
+// 定义回调函数
+function portraitCallBack(data,Qnumber) {
+    console.log('Data:');
+    console.table(data);
+    Qname = data[6];
+    console.log('Name');
+    console.log(Qname);
+    return data;
+}
+
+
+// // 使用字符串模板语法创建一个包含变量的字符串
+// const name = 'Alice';
+// const message = `Hello, ${name}!`;
+// console.log(message); // 输出 "Hello, Alice!"
+
+// // 使用ES6模板字面量创建一个包含变量的HTML元素
+// const age = 25;
+// const divElement = document.createElement('div');
+// divElement.innerHTML = `
+//   <p>Name: ${name}</p>
+//   <p>Age: ${age}</p>
+// `;
+// document.body.appendChild(divElement);
